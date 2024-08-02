31b8e43fc3ef011f103ba51fcf63d0a16b7ec879
Brayan Narvaez <brayannsaez77@gmail.com>
2024-08-02 15:24:39 -0500

    Realizamos un sistema que determine si el cliente puede ingresar o no a la discoteca de acuerdo a su edad

---
+++ ingreso-permitido.py
@@ -0,0 +1,41 @@
+
+edad = int(input("Por favor, ingresar la edad del cliente: "))
+
+permitido = True if edad >=18 else False #Ternario
+
+if permitido:
+    print("¡Puedes ingresar a la discoteca!")
+else:
+    print("Lo siento mucho, pero no puedes ingresa a la discoteca por ser menor de edad")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
